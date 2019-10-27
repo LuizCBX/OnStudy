09af2b0198617979d39d8ea5460116a6aebb9008
Antonio Felix <antonio.felixjr@hotmail.com>
2019-10-27 09:56:45 -0300

    sync

--- integrador/conexao.php
+++ integrador/conexao.php
@@ -1,7 +1,7 @@
 <?php
 define('SERVERNAME', 'localhost');
 define('USUARIO', 'root');
-define('SENHA', '');
+define('SENHA', 'Teste@12');
 define('DB', 'db_ead');
 
 $conexao = mysqli_connect(SERVERNAME, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
