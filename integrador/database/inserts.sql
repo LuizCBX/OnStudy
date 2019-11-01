bd70d81ed0a67c5b6394aef20e63247c7dcec4d5
Luiz Henrique <43183856+LuizCBX@users.noreply.github.com>
2019-11-01 16:34:55 -0300

    upload database

--- integrador/database/inserts.sql
+++ integrador/database/inserts.sql
@@ -3,23 +3,40 @@ INSERT INTO tb_usuario (usuario, senha) VALUES ('123','admin');
 INSERT INTO tb_usuario (usuario, senha) VALUES ('antoniofelix','123123');
 
 INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, estado) 
-VALUES ('Rua Jose',666, 'apt 23', 'Alphaville', '0812126', 'São Paulo', 'SP');
+VALUES ('Rua Paulo',666, 'apt 23', 'Anguera', '0812126', 'Bahia', 'SP');
 
 INSERT INTO tb_contato (email, celular) 
-VALUES ('luiz@bol.com', '(11)98582825');
+VALUES ('antonio@bol.com', '(11)959595');
 
 INSERT INTO tb_contato (email, celular) 
 VALUES ('antonio@gmail.com', '(11)960252423');
 
-INSERT INTO tb_nota(bimestre_1, bimestre_2, bimestre_3, bimestre_4) 
-VALUES(10, 9.0, 8.0, 7.0);
+INSERT INTO tb_aluno (ra, nome, cpf, dt_nasc, id_usuario, id_end, id_contato) VALUES (30100,'Luiz','55555555', '2000-12-01', 1, 1, 1);
+INSERT INTO tb_aluno (ra, nome, cpf, dt_nasc, id_usuario, id_end, id_contato) VALUES (30101, 'Antônio','12345612', '1996-10-01', 2, 2, 2);
 
-INSERT INTO tb_curso(nome, descricao, id_nota) 
-VALUES('Informática', 'Manutenção desktops', 1);
 
-INSERT INTO tb_aluno (nome, cpf, dt_nasc, id_usuario, id_end, id_contato, id_curso) VALUES ('Carioca','55555555', '11/12/2000', 2, 1, 1, 1);
-INSERT INTO tb_aluno (nome, cpf, dt_nasc, id_usuario, id_end, id_contato, id_curso) VALUES ('Antônio Felix da Silva Junior','12312345612', '01/10/1996', 3, 1, 1, 1);
+INSERT INTO tb_curso(nome, descricao, nota_minima) 
+VALUES('Lógica de programação', '5 aulas', 7.0);
 
-INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, estado) 
-VALUES ('Rua Jose',666, 'apt 23', 'Alphaville', '0812126', 'São Paulo', 'SP');
+INSERT INTO tb_curso(nome, descricao, nota_minima) 
+VALUES('Java', '5 aulas', 7.0);
+
+INSERT INTO tb_aula(nome, descricao, id_curso) 
+VALUES('Algoritmos', 'Estrutura Básica', 1);
+
+INSERT INTO tb_aula(nome, descricao, id_curso) 
+VALUES('POO', 'Estrutura Básica', 2);
+
+
+INSERT INTO tb_matricula(id_aluno, id_curso) 
+VALUES(1, 1);
+
+INSERT INTO tb_questao(questao, id_curso) 
+VALUES("2+2x2", 1);
+
+INSERT INTO tb_alternativa(at_1, at_2, at_3, at_4, at_correta, id_questao) 
+VALUES('5','2','4','6','6', 1);
+
+INSERT INTO tb_avaliacao( nota_final, id_aluno, id_curso) 
+VALUES(8.8, 1, 1);
 
--- integrador/database/structure.sql
+++ integrador/database/structure.sql
@@ -4,15 +4,15 @@ USE db_ead;
 
 DROP TABLE IF EXISTS `tb_usuario`;
 CREATE TABLE tb_usuario(
-  id_usuario INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   usuario VARCHAR(200) NOT NULL,
   senha VARCHAR(32) NOT NULL,
-  PRIMARY KEY (id_usuario)
-);
+  PRIMARY KEY (id)
+)engine InnoDB;
 
 DROP TABLE IF EXISTS `tb_endereco`;    
 CREATE TABLE tb_endereco(
-	id_end int auto_increment primary key,
+	id int auto_increment primary key,
     logradouro varchar(40),
     numero int,
     complemento varchar(40),
@@ -20,75 +20,92 @@ CREATE TABLE tb_endereco(
     cep varchar(15),
     cidade varchar(50),
     estado varchar(50)
-);
+)engine InnoDB;
 
 DROP TABLE IF EXISTS `tb_contato`;
 CREATE TABLE tb_contato(
-	id_contato int auto_increment primary key,
+	id int auto_increment primary key,
     email varchar(50),
     celular varchar(30)
-);
+)engine InnoDB;
 
-DROP TABLE IF EXISTS `tb_curso`;
-CREATE TABLE tb_curso(
-	id_curso int auto_increment,
-    nome varchar(70) not null,
-    descricao varchar(200),
-    id_nota int not null,
-    PRIMARY KEY(id_curso)
-);
 
 DROP TABLE IF EXISTS `tb_aluno`;
 CREATE TABLE tb_aluno(
-	ra int auto_increment primary key,
+	id int auto_increment primary key,
+    ra int unique not null,
     nome varchar(100) not null,
     cpf varchar(15) not null,
-    dt_nasc varchar (20) not null,
+    dt_nasc date not null,
     id_usuario int not null unique,
     id_end int not null,
     id_contato int not null,
+	constraint fk_usuario_pk_aluno foreign key (id_usuario) references tb_usuario(id),
+	constraint fk_end_pk_aluno foreign key (id_end) references tb_endereco(id),
+    constraint fk_ct_pk_aluno foreign key (id_contato) references tb_contato(id)
+)engine InnoDB;
+
+DROP TABLE IF EXISTS `tb_curso`;
+CREATE TABLE tb_curso(
+	id int auto_increment,
+    nome varchar(70) not null,
+    descricao text,
+    nota_minima decimal (5,2),
+    PRIMARY KEY(id)
+)engine InnoDB;
+
+
+DROP TABLE IF EXISTS `tb_aula`;
+CREATE TABLE tb_aula(
+	id int auto_increment PRIMARY KEY,
+	nome varchar(50) not null,
+    descricao text,
+	id_curso int not null,
+	constraint fk_curso_pk_aula foreign key (id_curso) references tb_curso(id)
+)engine InnoDB;
+
+DROP TABLE IF EXISTS `tb_matricula`;
+CREATE TABLE tb_matricula(
+	id int auto_increment PRIMARY KEY,
+    data_matricula timestamp default current_timestamp(),
+	id_aluno int not null,
+	id_curso int not null,
+	constraint fk_aluno_pk_matri foreign key (id_aluno) references tb_aluno(id),
+	constraint fk_curso_pk_matri foreign key (id_curso) references tb_curso(id)
+)engine InnoDB;
+
+
+DROP TABLE IF EXISTS `tb_questao`;
+CREATE TABLE tb_questao(
+	id int auto_increment PRIMARY KEY,
+    questao varchar(250),
     id_curso int not null,
-    constraint fk_usuario_pk_ra foreign key (id_usuario) references tb_usuario(id_usuario),
-	constraint fk_end_pk_ra foreign key (id_end) references tb_endereco(id_end),
-    constraint fk_ct_pk_ra foreign key (id_contato) references tb_contato(id_contato),
-	constraint fk_curso_pk_ra foreign key (id_curso) references tb_curso(id_curso)
-);
-
-DROP TABLE IF EXISTS `tb_nota`;
-CREATE TABLE tb_nota(
-	id_nota int auto_increment,
-    bimestre_1 decimal (5,2), 
-    bimestre_2 decimal (5,2),
-    bimestre_3 decimal (5,2),
-    bimestre_4 decimal (5,2),
-    PRIMARY KEY(id_nota)
-);
+    constraint fk_curso_pk_questao foreign key (id_curso) references tb_curso(id)
+)engine InnoDB;
 
 
--- Estrutura da tabela `events`
+DROP TABLE IF EXISTS `tb_alternativa`;
+CREATE TABLE tb_alternativa(
+	id int auto_increment PRIMARY KEY,
+    at_1 varchar (255), 
+    at_2 varchar (255), 
+    at_3 varchar (255), 
+    at_4 varchar (255), 
+    at_correta varchar (10), 
+	id_questao int not null,	
+    constraint fk_questao_pk_at foreign key (id_questao) references tb_questao(id)
+)engine InnoDB;
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE IF NOT EXISTS `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(220) DEFAULT NULL,
-  `color` varchar(10) DEFAULT NULL,
-  `start` datetime DEFAULT NULL,
-  `end` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
---
--- Extraindo dados da tabela `events`
---
+DROP TABLE IF EXISTS `tb_avaliacao`;
+CREATE TABLE tb_avaliacao(
+	id int auto_increment PRIMARY KEY,
+    data_av timestamp default current_timestamp(),
+    nota_final decimal (5,2),
+	id_aluno int not null,
+    id_curso int not null,
+	constraint fk_aluno_pk_av foreign key (id_aluno) references tb_aluno(id),
+	constraint fk_curso_pk_av foreign key (id_curso) references tb_curso(id)
+)engine InnoDB;
 
-INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
-(1, 'Tutorial PagSeguro', '#FFD700', '2019-05-21 15:00:00', '2019-05-21 16:00:00'),
-(2, 'Tutorial FullCalendar editar detalhes do evento', '#0071c5', '2019-05-30 15:00:00', '2019-05-30 00:00:00'),
-(3, 'Tutorial FullCalendar Visualizar detalhes do evento', '#0071c5', '2019-05-23 15:00:00', '2019-05-23 16:00:00'),
-(4, 'Reuniao 3', '#40e0d0', '2019-05-17 17:00:00', '2019-05-17 18:00:00'),
-(5, 'Reuniao 4', '#0071c5', '2019-05-17 15:00:00', '2019-05-17 16:00:00'),
-(6, 'Reuniao 5', '#FFD700', '2019-05-17 13:00:00', '2019-05-17 14:00:00'),
-(7, 'Reuniao 6', '#0071c5', '2019-05-17 11:00:00', '2019-05-17 12:00:00'),
-(8, 'Reuniao 7', '#40e0d0', '2019-05-17 09:00:00', '2019-05-17 10:00:00'),
-(9, 'Tutorial', '#228B22', '2019-09-12 00:00:00', '2019-09-13 00:00:00'),
-(10, 'Tutorial 2', '#FF4500', '2019-09-13 15:10:10', '2019-09-13 17:15:15');
-COMMIT;
+
+
