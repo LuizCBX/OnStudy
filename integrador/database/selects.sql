1e3f67cc5bb8559694c7daf52907dacba83f5a01
Luiz Henrique <43183856+LuizCBX@users.noreply.github.com>
2019-11-05 11:43:47 -0300

    add select

--- integrador/database/selects.sql
+++ integrador/database/selects.sql
@@ -5,3 +5,9 @@ select * from tb_nota;
 select * from tb_endereco;
 
 select us.usuario, al.nome from tb_usuario us inner join tb_aluno al on us.id_usuario=al.ra where usuario = '12345';
+
+
+
+SELECT u.usuario, a.nome, a.ra, c.nome, c.descricao FROM tb_usuario AS u 
+INNER JOIN tb_aluno AS a ON a.id = u.id 
+INNER JOIN tb_curso AS c ON c.id = u.id;
