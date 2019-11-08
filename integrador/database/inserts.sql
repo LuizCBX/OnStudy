d2b4871deb621f53eab97ada4bb1280eb1ce2d9c
Luiz Henrique <43183856+LuizCBX@users.noreply.github.com>
2019-11-08 17:27:55 -0300

    Inserindo dados e selects

--- integrador/database/inserts.sql
+++ integrador/database/inserts.sql
@@ -2,10 +2,11 @@ INSERT INTO tb_usuario (usuario, senha) VALUES ('111','admin');
 INSERT INTO tb_usuario (usuario, senha) VALUES ('123','admin');
 INSERT INTO tb_usuario (usuario, senha) VALUES ('antoniofelix','123123');
 
-INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, estado) 
+INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, uf) 
 VALUES ('Rua Paulo',666, 'apt 23', 'Anguera', '0812126', 'Bahia', 'SP');
-INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, estado) 
-VALUES ('Rua Ângelo Pedroso',132, 'apt 12', 'Guaianases', '08460345', 'São Paulo', 'SP');
+
+INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade, uf) 
+VALUES ('Rua Paulo',777, 'apt 32', 'Teste', '08151', 'Rio de Janeiro', 'SP');
 
 INSERT INTO tb_contato (email, celular) 
 VALUES ('antonio@bol.com', '(11)959595');
@@ -14,7 +15,7 @@ INSERT INTO tb_contato (email, celular)
 VALUES ('antonio@gmail.com', '(11)960252423');
 
 INSERT INTO tb_aluno (ra, nome, cpf, dt_nasc, id_usuario, id_end, id_contato) VALUES (30100,'Luiz','55555555', '2000-12-01', 1, 1, 1);
-INSERT INTO tb_aluno (ra, nome, cpf, dt_nasc, id_usuario, id_end, id_contato) VALUES (30101, 'Antônio','12345612', '1996-10-01', 2, 2, 2);
+INSERT INTO tb_aluno (ra, nome, cpf, dt_nasc, id_usuario, id_end, id_contato) VALUES (30102, 'Antônio','12345612', '1996-10-01', 2, 2, 2);
 
 
 INSERT INTO tb_curso(nome, descricao, nota_minima) 
@@ -23,8 +24,17 @@ VALUES('Lógica de programação', '5 aulas', 7.0);
 INSERT INTO tb_curso(nome, descricao, nota_minima) 
 VALUES('Java', '5 aulas', 7.0);
 
+INSERT INTO tb_curso(nome, descricao, nota_minima) 
+VALUES('Excel Avançado', '7 aulas', 7.0);
+
 INSERT INTO tb_aula(nome, descricao, id_curso) 
 VALUES('Algoritmos', 'Estrutura Básica', 1);
+
+INSERT INTO tb_aula(nome, descricao, id_curso) 
+VALUES('Matemática Aplicada', 'Estrutura Básica', 1);
+
+INSERT INTO tb_aula(nome, descricao, id_curso) 
+VALUES('Lógica com Python', 'Estrutura Básica', 1);
 
 INSERT INTO tb_aula(nome, descricao, id_curso) 
 VALUES('POO', 'Estrutura Básica', 2);
@@ -33,10 +43,23 @@ VALUES('POO', 'Estrutura Básica', 2);
 INSERT INTO tb_matricula(id_aluno, id_curso) 
 VALUES(1, 1);
 
-INSERT INTO tb_questao(descricao, id_curso, alternativa_a, alternativa_b, alternativa_c, 
-alternativa_d, alternativa_e, alternativa_correta) 
-VALUES("2+2x2", 1, '5','2','4','0','6', 'e');
+INSERT INTO tb_matricula(id_aluno, id_curso) 
+VALUES(1, 2);
 
-INSERT INTO tb_avaliacao( nota_final, id_aluno, id_curso, id_questao) 
-VALUES(8.8, 1, 1, 1);
+INSERT INTO tb_matricula(id_aluno, id_curso) 
+VALUES(1, 3);
+
+INSERT INTO tb_questao(questao, id_curso) 
+VALUES("2+2x2", 1);
+
+INSERT INTO tb_alternativa(at_1, at_2, at_3, at_4, at_correta, id_questao) 
+VALUES('5','2','4','6','6', 1);
+
+INSERT INTO tb_avaliacao( nota_final, id_aluno, id_curso) 
+VALUES(8.8, 1, 1);
+
+
+
+
+
 
--- integrador/database/selects.sql
+++ integrador/database/selects.sql
@@ -1,13 +1,21 @@
 select * from tb_usuario;
 select * from tb_aluno;
 select * from tb_curso;
-select * from tb_nota;
 select * from tb_endereco;
+select * from tb_contato;
+select * from tb_matricula;
+select * from tb_aula;
+select * from tb_questao;
+select * from tb_alternativa;
+
 
 select us.usuario, al.nome from tb_usuario us inner join tb_aluno al on us.id_usuario=al.ra where usuario = '12345';
 
 
+select u.id, u.usuario,a.nome, a.ra from tb_usuario u inner join tb_aluno a on u.id=a.id_usuario where u.usuario = '{$usuario}' and senha = '{$senha}';
 
-SELECT u.usuario, a.nome, a.ra, c.nome, c.descricao FROM tb_usuario AS u 
-INNER JOIN tb_aluno AS a ON a.id = u.id 
-INNER JOIN tb_curso AS c ON c.id = u.id;
+
+SELECT c.nome as curso, c.descricao, a.id, a.nome as aula, a.descricao from tb_aula a INNER JOIN tb_curso c on a.id_curso=c.id where c.id=1; 
+
+select c.nome,c.descricao,c.nota_minima from tb_aluno a inner join tb_matricula m on a.id=m.id_aluno inner join tb_curso c on c.id=m.id_curso where a.id_usuario=1;
+
