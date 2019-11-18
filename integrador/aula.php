8a6eb56bcb51f9cd9a54db6aa92cde725a2d96cc
Luiz Henrique <43183856+LuizCBX@users.noreply.github.com>
2019-11-18 16:47:37 -0300

    Add files via upload

--- integrador/aula.php
+++ integrador/aula.php
@@ -2,6 +2,22 @@
 $cursoDoAluno = $alunos->getAulaPorCurso($_GET['id_curso']);
 ?>
 
+
+<script>
+    function abrir(identificador){
+        document.getElementById(identificador).style.display="block";
+    }
+
+    function fecharAula(fechada){
+        document.getElementById(fechada).style.display="none";
+    }
+
+    
+
+</script>
+
+
+
 <div class="back">
 
     id do curso <?php echo $_GET['id_curso']; ?>
@@ -10,14 +26,40 @@ $cursoDoAluno = $alunos->getAulaPorCurso($_GET['id_curso']);
     <?php
         echo "<ol>";
             while($linha = mysqli_fetch_assoc($cursoDoAluno))   { 
+    ?>          
+                <br>
+                <?php
+                echo "<li><button class='btn btn-primary' onclick=abrir('aula".$linha['id']."');>".$linha['aula']
+                
+                ."</button></li>";
+
+                
+                echo "<div class='teste' id=aula".$linha['id']." style=\"display:none\">";
+                echo $linha['descricao'];
+                ?>
+
+                <br>
+                <?php
+                echo $linha['url_video'];
+                echo "</div>";
+
+                
             
-                echo "<li>".$linha['aula']."</li>";
 
             }
 
         echo "</ol>";
     ?>
-    
 
+
+
+
+    <div>
+
+        <!-- .card
+	    .card-play
+	    .card-video
+		iframe#video(src="https://www.youtube.com/embed/JE9z-gy4De4?enablejsapi=1&html5=1&iv_load_policy=3&rel=0&showinfo=0", frameborder="0", allowfullscreen) -->
+    <div>
 
 <?php include 'footer.php';?>
